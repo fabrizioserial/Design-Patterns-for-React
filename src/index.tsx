2353822e84fb3497f5185878eb1ccd917f3195db
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2022-07-31 17:14:10 -0300

    feat: add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import ProblemSRP from "./solid/single-responsability-principle/problem";
+import SolutionSRP from "./solid/single-responsability-principle/solution";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path={'/solid/single-responsibility-principle/problem'} element={<ProblemSRP/>}/>
+              <Route path={'/solid/single-responsibility-principle/solution'} element={<SolutionSRP/>}/>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
